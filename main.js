62fe6672fe28d9cf940d650c06ed98f4e9617ccd
Diogo Lopes <40220401@esmad.ipp.pt>
2023-04-14 11:15:32 +0100

    glitch upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-alert("Bem vindo á sala de cinema\nControlos:\nU - Inserir video local\nP - Iniciar/Pausar video")
+alert("Bem vindo á sala de cinema\n\nControlos:\nU - Inserir video local\nP - Iniciar/Pausar video")
 
 const video = document.querySelector("#video-player");
+
+let fov = 100;
+fov = parseInt(fov)
 
 
 document.addEventListener("keydown",play)
@@ -17,7 +20,8 @@ function play(event){
 document.addEventListener("keydown",external)
 function external(event){
     if(event.key == "u"){
-        let link = prompt("Insira o link do vídeo do youtube.")
+        let link = prompt("Insira o nome do ficheiro a abrir\n(O ficheiro tem de estar na pasta 'Video' e ser .mp4")
+        link = "/Video/"+link+".mp4"
         video.src = link
         delete link
     }
