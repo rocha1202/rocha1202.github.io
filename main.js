dd821a31cd070f14da8ffe5744b9f7b4ee3946b0
DiogoLopess <40220401@esmad.ipp.pt>
2023-04-16 16:13:47 +0100

    Commit Final( Sem Landing Page)

--- main.js
+++ main.js
@@ -1,31 +1,36 @@
-alert("Bem vindo á sala de cinema\n\nControlos:\nU - Inserir video local\nP - Iniciar/Pausar video")
-
 const video = document.querySelector("#video-player");
 
 let fov = 100;
 fov = parseInt(fov)
 
-
-document.addEventListener("keydown",play)
-function play(event){
-    if(event.key == "p"){
-        if(video.paused == true){
-            video.play()
-        }else{
-            video.pause()
-        }
+AFRAME.registerComponent("controlevideo", {
+    init: function () {
+      this.el.addEventListener("mouseenter", function (evt) {
+        $("#video-player").trigger('play')
+      })
+      this.el.addEventListener("mouseleave", function (evt) {
+        $("#video-player").trigger('pause')
+      })
     }
-}
+})
 
-document.addEventListener("keydown",external)
-function external(event){
-    if(event.key == "u"){
-        let link = prompt("Insira o nome do ficheiro a abrir\n(O ficheiro tem de estar na pasta 'Video' e ser .mp4")
-        link = "/Video/"+link+".mp4"
-        video.src = link
-        delete link
-    }
-}
 
-setTimeout(() => {
-}, 10000);  
+//Spider-man
+AFRAME.registerComponent("poster1", {
+    init: function () {
+    this.el.addEventListener("mouseenter", function (evt) {
+        video.src = "/Video/spider-man.mp4"
+    })
+}
+})
+
+
+
+//Titanic
+AFRAME.registerComponent("poster2", {
+    init: function () {
+    this.el.addEventListener("mouseenter", function (evt) {
+        video.src = "/Video/titanic.mp4"
+    })
+}
+})
